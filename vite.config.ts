061ff1998703f73fb1068cf44af2762b43191d85
bmancilla23 <bmancilla119@gmail.com>
2024-04-19 17:42:58 -0500

    chore: alias ​​settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+  resolve: {
+    alias: {
+      "@common": path.resolve(__dirname, "src/common"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@styles": path.resolve(__dirname, "src/styles")
+    }
+  }
+});
