2bb2655ce1031a6bc8e25ebdb0c2b7a30860d454
bmancilla23 <bmancilla119@gmail.com>
2024-05-10 13:22:40 -0500

    chore: update alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as path from 'path'
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@common": path.resolve(__dirname, "src/common"),
-      "@components": path.resolve(__dirname, "src/components"),
-      "@pages": path.resolve(__dirname, "src/pages"),
-      "@assets": path.resolve(__dirname, "src/assets"),
-      "@styles": path.resolve(__dirname, "src/styles")
-    }
-  }
+      '@': path.resolve(__dirname, './src/'),
+      components: `${path.resolve(__dirname, './src/components/')}`,
+      public: `${path.resolve(__dirname, './public/')}`,
+      pages: path.resolve(__dirname, './src/pages'),
+      types: `${path.resolve(__dirname, './src/@types')}`,
+      hooks: `${path.resolve(__dirname, './src/hooks')}`,
+      api: `${path.resolve(__dirname, './src/api')}`,
+      utils: `${path.resolve(__dirname, './src/utils')}`,
+      ui: `${path.resolve(__dirname, './src/ui')}`,
+    },
+  },
 });
